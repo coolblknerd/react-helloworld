eddfc260b82a118c13c44828fd21fcd6cee21dd5
Reggie Davis <reginald.davis88@gmail.com>
2019-01-20 02:02:09 -0600

    Added enzyme for testing and updated App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme'
 
+// Shallow rendering is used for isolated unit tests
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />)
 });
