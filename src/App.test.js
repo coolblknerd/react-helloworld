31131993fe1371a65d92b9162c949b7f91d7e7e6
Reggie Davis <reginald.davis88@gmail.com>
2019-01-20 11:41:35 -0600

    Update test to check if components are rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import App from './App';
-import {shallow} from 'enzyme'
+import {shallow} from 'enzyme';
+import {shallowToJson} from 'enzyme-to-json';
 
 // Shallow rendering is used for isolated unit tests
-it('renders without crashing', () => {
-  shallow(<App />)
+describe('App', () => {
+  it('should render correctly', () => {
+    const output = shallow(
+      <App />
+    );
+    expect(shallowToJson(output)).toMatchSnapshot();
+  });
+
+  it('should render button that says "Click Me"', () => {
+    const output = shallow(
+      <App />
+    );
+    expect(output.find('button').text()).toEqual('Click Me');
+  });
+
+  // Really want to know how to test event
 });
+
